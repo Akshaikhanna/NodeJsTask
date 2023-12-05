ba27397b847998a20390dd6d164a6735ad76f17b
Akshaikhanna <akshaikhanna123@gmail.com>
2023-12-05 09:53:17 +0530

    passport

--- passport.js
+++ passport.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const app = express();
 const passport = require("passport");
 const session = require("express-session");
 const LocalStrategy = require("passport-local").Strategy;
-
-const app = express();
 
 //middleware
 app.use(express.json());
